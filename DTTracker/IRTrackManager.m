6a9b880c3a01278cd7eb9835d0a4b99da1884852
@iOSRunner <@ericguo1213@163.com>
2017-07-19 16:14:59 +0800

    init file

---
+++ DTTracker/IRTrackManager.m
@@ -0,0 +1,17 @@
+//
+//  DTTrackManager.m
+//  DTTracker
+//
+//  Created by Wanlei on 2017/5/17.
+//  Copyright © 2017年 organizer. All rights reserved.
+//
+
+#import "IRTrackManager.h"
+
+@implementation IRTrackManager
+
+- (void)trackEvent:(NSString *)event {
+    NSLog(@"you are tracking event: %@", event);
+}
+
+@end
